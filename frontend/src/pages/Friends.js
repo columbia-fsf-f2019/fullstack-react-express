9aa67485835aec24f422be7990e7bc4c001228d3
Michael Gelber <mpgelber7495@gmail.com>
2020-03-21 11:25:29 -0400

    Feature: Friend cards display at /friends route

--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import Card from "../components/Card";
 
-const Friends = () => {
-  return <h1>Friends</h1>;
-};
+class Friends extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { friends: [] };
+  }
+  async componentWillMount() {
+    const response = await axios.get("http://localhost:3001/api/users");
+    const friends = response.data;
+    this.setState({ friends });
+  }
+  render() {
+    return (
+      <div className="ui link cards">
+        {this.state.friends.length > 0
+          ? this.state.friends.map(friend => {
+              return (
+                <Card
+                  key={friend.id}
+                  name={friend.name}
+                  location={friend.location}
+                  occupation={friend.occupation}
+                  img={friend.image}
+                />
+              );
+            })
+          : ""}
+      </div>
+    );
+  }
+}
 
 export default Friends;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const morgan = require('morgan');
-const db = require('./db');
+const express = require("express");
+const bodyparser = require("body-parser");
+const morgan = require("morgan");
+const db = require("./db");
+var cors = require("cors");
 
 /* constants */
 const PORT = process.env.PORT || 3001;
@@ -10,14 +11,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 /* set-up middlewares */
-app.use(morgan('dev')); // logging
+app.use(cors());
+app.use(morgan("dev")); // logging
 app.use(bodyparser.json()); // parsing json data in request body
 
 /* route handlers */
-app.get('/api/ping', (req, res) => res.send('pong'));
+app.get("/api/ping", (req, res) => res.send("pong"));
 
 app
-  .route('/api/users')
+  .route("/api/users")
   .get((req, res) => res.json(db.users))
   .post((req, res) => {
     const newUserId = db.users.length + 1;

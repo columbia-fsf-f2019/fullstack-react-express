d092368780470e0e79edfa1f4dd48deced34ab43
russomp <russo.michaelp@gmail.com>
2020-03-21 10:15:17 -0400

    feat: Set up preliminary server and user routes

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+const db = require('./db');
+
+/* constants */
+const PORT = process.env.PORT || 3001;
+
+/* */
+const app = express();
+
+/* set-up middlewares */
+app.use(morgan('dev')); // logging
+app.use(bodyparser.json()); // parsing json data in request body
+
+/* route handlers */
+app.get('/api/ping', (req, res) => res.send('pong'));
+
+app
+  .route('/api/users')
+  .get((req, res) => res.json(db.users))
+  .post((req, res) => {
+    const newUserId = db.users.length + 1;
+    db.users.push({ ...req.body, id: newUserId });
+    res.status(201).json(newUserId);
+  });
+
+/* run our app */
+app.listen(PORT, () => {
+  console.log(`[SERVER] app listening on port ${PORT}`);
+});
